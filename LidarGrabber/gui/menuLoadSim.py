f150c2b73921621bdf1b81c9e9fa8567bf474033
marsberry <marsberry@cu.ac.kr>
2020-08-20 21:35:15 +0900

    change log  - gui updated for play log  - tool architecture is updated

---
+++ LidarGrabber/gui/menuLoadSim.py
@@ -0,0 +1,19 @@
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import qApp, QAction, QMainWindow
+
+from gui.menuItem import MenuItem
+from simMode import Mode
+
+
+class menuSim(QAction):
+
+    def __init__(self, name, parent):
+        super().__init__(name, parent)
+        self.parent = parent
+        self.triggered.connect(self.trig)
+        self.setShortcut('Ctrl+P')
+        self.setStatusTip('Exit application')
+
+    def trig(self):
+        self.parent.simulator.setAction(Mode.MODE_SIM)
+        print("PlaySim")
