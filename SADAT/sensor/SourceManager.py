e513318324432fef2b88b581eef6524f39934d25
marsberry <marsberry@cu.ac.kr>
2020-12-02 02:42:21 +0900

    Added classes for managing multi sensor sources
    Former-commit-id: cbc55d8357e92e1ae4fe488d9e252bf0552e885f

---
+++ SADAT/sensor/SourceManager.py
@@ -0,0 +1,7 @@
+class SourceManager:
+    def __init__(self):
+        self.Lidar2Detection = None
+        self.Lidar3Detection = None
+        self.Track = None
+        self.IMU = None
+        self.VehicleData = None
---
+++ SADAT/views/DataView.py
@@ -0,0 +1,5 @@
+class DataView:
+    def __init__(self):
+        self.isVisible = False
+        self.viewType = None
+        self.RawData = None
